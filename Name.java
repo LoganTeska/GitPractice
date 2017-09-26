8377a3cc1bf2f077ba17ead1cc2297ac6fd8f18c
LoganTeska <32313691+LoganTeska@users.noreply.github.com>
2017-09-26 18:56:56 -0400

    Update Name.java
    added suffixProfessional

--- Name.java
+++ Name.java
@@ -62,6 +62,7 @@ public class Name {
 
     public String getSuffixProfessional() {
     	/* return Name's professional suffix */
+        return suffixProfessional;
     }
 
     public String getSuffixGenerational() {
